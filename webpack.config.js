41a58a9c828a7b24d4fb203831e4d55ce46a0537
JRG-zhangshijun <462895506@qq.com>
2017-08-04 22:18:33 +0800

    第一次上传

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
