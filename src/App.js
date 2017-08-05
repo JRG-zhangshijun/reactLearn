43b29b63f77c1b1543f4e03b924699753d52962b
JRG-zhangshijun <462895506@qq.com>
2017-08-05 14:42:27 +0800

    install leancloud

--- src/App.js
+++ src/App.js
@@ -1,21 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import 'normalize.css'
+import './reset.css'
 import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+import * as localStore from './localStore'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTodo: '',
+            todoList: localStore.load('todoList') || []
+        }
+    }
+
+    render() {
+        let todos = this.state.todoList
+            .filter((item) => !item.delete)
+            .map((item, index) => {
+                return (
+                    <li key={index}>
+                        <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+                                  onDelete={this.delete.bind(this)}/>
+                    </li>
+                )
+            })
+        return (
+            <div className="App">
+                <h1>我的待办</h1>
+                <div className="inputWrapper">
+                    <TodoInput content={this.state.newTodo}
+                               onChange={this.changeTitle.bind(this)}
+                               onSubmit={this.addTodo.bind(this)}/>
+                </div>
+                <ol className="todoList">
+                    {todos}
+                </ol>
+            </div>
+        )
+    }
+
+    componentDidUpdate() {
+        localStore.save('todoList', this.state.todoList)
+    }
+
+    toggle(e, todo) {
+        todo.status = todo.status === 'completed' ? '' : 'completed'
+        this.setState(this.state)
+    }
+
+    changeTitle(event) {
+        this.setState({
+            newTodo: event.target.value,
+            todoList: this.state.todoList
+        })
+    }
+
+    addTodo(event) {
+        console.log(this)
+        this.state.todoList.push({
+            id: idMark(),
+            title: event.target.value,
+            status: null,
+            deleted: false
+        })
+        this.setState({
+            newTodo: '',
+            todoList: this.state.todoList
+        })
+    }
+
+    delete(event, todo) {
+        todo.delete = true
+        this.setState(this.state)
+    }
 }
 
 export default App;
+
+let id = 0
+
+function idMark() {
+    id += 1
+    return id
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(
-    <h1>hello,world</h1>,
+    <App/>,
     document.getElementById('root')
 )
